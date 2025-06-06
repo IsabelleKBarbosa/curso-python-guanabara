78a588c3071cdf92db1d41aa0c792dbdbe5a7e97
Isabelle Barbosa <bllk.barbosa@gmail.com>
2025-06-06 10:01:51 -0300

    Create Desafio_03.py

---
+++ Mundo_01/Aulas_01_a_05_Primeiros_Passos/Desafio_03.py
@@ -0,0 +1,10 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles
+
+print ('_____________________________________________________________________')
+print ('## ATIVIDADE PRÁTICA ##')
+print ('Vamos fazer a soma de dois números inteiros quaisquer' + '\n')
+n1 = int(input ('Digite o primeiro número: '))
+n2 = int(input ('Digite o segundo número: '))
+soma = n1 + n2
+print()
+print(f'A soma entre {n1} e {n2} vale {soma}')
