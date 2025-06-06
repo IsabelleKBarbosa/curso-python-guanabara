3242a21123b8402d4edbdcc1e5e4fef205df19a9
Isabelle Barbosa <bllk.barbosa@gmail.com>
2025-06-06 11:48:43 -0300

    Create Desafio_07.py

---
+++ Mundo_01/Aulas_06_e_07_Entrada_Saida_Operadores/Desafio_07.py
@@ -0,0 +1,9 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+print ('_____________________________________________________________________')
+print ('## ATIVIDADE PRÁTICA ##')
+print ()
+nota1 = float (input('Forneça a primeira nota do estudante: '))
+nota2 = float (input('Forneça a segunda nota do estudante: '))
+media = (nota1 + nota2) / 2
+print()
+print (f'A media do(a) estudante é: {media:.1f}')
