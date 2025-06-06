7db5e015b6272bd525e83aac1cc0f9d67a26f357
Isabelle Barbosa <bllk.barbosa@gmail.com>
2025-06-06 10:00:28 -0300

    Create Desafio_01_02.py

---
+++ Mundo_01/Aulas_01_a_05_Primeiros_Passos/Desafio_01_02.py
@@ -0,0 +1,7 @@
+# Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+
+print ()
+print ('Vamos fazer o primeiro exercício!')
+print ()
+nome = input ('Digite o seu nome: ')
+print(f'É um prazer te conhecer, {nome}. Seja bem vindo(a)!')
